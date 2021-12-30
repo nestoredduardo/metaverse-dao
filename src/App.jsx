c73fbf2c07b66d5e0a8921ade2145ebdcf1a1efc
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-30 11:23:49 -0500

    Connect your wallet button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useWeb3 } from '@3rdweb/hooks'
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3()
+  console.log('👋 Address:', address)
+
   return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      {!address ? (
+        <>
+          <h1 className="text-2xl">Welcome to MetaverseDAO</h1>
+          <button
+            onClick={() => connectWallet('injected')}
+            className="px-4 py-2 bg-blue-500 hover:bg-blue-700 rounded-md mt-8 font-bold text-white"
+          >
+            Connect your wallet
+          </button>
+        </>
+      ) : (
+        <h1>👀 wallet connected, now what!</h1>
+      )}
+    </main>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 
 import App from './App'
 
 import './index.css'
 
+const supportedChainIds = [4]
+
+const connectors = {
+  injected: {},
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
